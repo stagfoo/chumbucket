e0da75153f8f3650fd4ced7a980b2fe1b1b73133
Stagfoo <alexanderddking@gmail.com>
2020-07-09 09:22:30 +0900

    Toast and tidy :)

--- src/store.ts
+++ src/store.ts
@@ -1,24 +1,31 @@
 import { reducer } from 'obake.js';
 
 export const defaultState = {
-  greeting: '🍖',
-  currentPage: { name: 'HOME' }
+  greeting: '🍖🍖🍖🍖',
+  currentPage: { name: 'HOME', activePage: "/" },
+  notification: {
+    text: "sharks loaded",
+    show: true
+  }
  }
 
  export const routes = {
     'Home': '/',
-    'Another Page': '/another-page',
+    'Example Fetch': '/example-fetch',
  }
- export const navbarIcons = {
-  'Home': 'home',
-  'Another Page': 'search',
+ //TODO clean up
+ export const activePage = {
+   "HOME": "/",
+   "EXAMPLE_FETCH": "/example-fetch"
  }
-
 export const reducers = {
-  updateCurrentPage: reducer((state, value) => {
-    state.currentPage = { name: value };
+  updateCurrentPage: reducer((state, value: string) => {
+    state.currentPage = { name: value, activePage:activePage[value]  };
   }),
-  changeGreeting: reducer((state, value) => {
+  changeGreeting: reducer((state, value: string) => {
     state.greeting = value;
+  }),
+  updateNotification: reducer((state, value:{text: string, show: boolean}) => {
+    state.notification = value;
   }),
 }
--- src/styles.ts
+++ src/styles.ts
@@ -21,7 +21,7 @@ export const DS = {
   colors: {
     purple: "#8D58FD",
     blue: "#58D5FD",
-    green: "#79E35E",
+    green: "#83FD58",
   }
 }
 
@@ -36,7 +36,29 @@ function buttonStyle(){
   font-size: ${DS.fontSizes.lg}px;
   padding: ${DS.gutters.md}px;
   border: 0;
+  text-decoration: none;
   `
+}
+export function notificationStyle(){
+  STYLES.add("notificationStyle", `
+  .notification {
+    background: #fff;
+    box-shadow: 10px 10px 0px #000;
+    position:fixed;
+    font-size: ${DS.fontSizes.md}px;
+    padding: ${DS.gutters.md}px;
+    width: 320px;
+    text-align:center;
+    transition: ease all 0.3s;
+    animation-name: notification;
+    animation-duration: 0.3s;
+    }
+  .notification.hide {
+    animation-name: notification-out;
+    animation-duration: 0.3s;
+    animation-fill-mode: forwards;
+  }
+  `)
 }
 
 export function BaseStyles() {
@@ -46,6 +68,7 @@ export function BaseStyles() {
       padding: 0;
       background: ${DS.colors.purple};
       color:  ${DS.colors.blue};
+      opacity: 1;
     }
     textarea,
     button {
@@ -54,13 +77,32 @@ export function BaseStyles() {
       margin: ${DS.gutters.sm}px auto ${DS.gutters.sm}px auto;
       ${buttonStyle()}
     }
-    body {
-      opacity: 1;
-    }
     textarea {
       width: 100%;
       min-height: 300px;
       font-size: ${DS.fontSizes.xl}px;
+    }
+    .nav {
+      margin-top: ${DS.gutters.xl}px;
+    }
+    .nav li {
+      display:inline-block;
+    }
+    .nav li.active a {
+      background: ${DS.colors.green};
+      color:  ${DS.colors.purple};
+    }
+    .nav li a {
+      ${buttonStyle()}
+      margin-right: ${DS.gutters.sm}px;
+    }
+    @keyframes notification {
+      from {bottom: -20vh;}
+      to {bottom: 5vh;}
+    }
+    @keyframes notification-out {
+      to {bottom: -20vh; display:none;}
+      from {bottom: 5vh; display:block;}
     }
   `)
 }
