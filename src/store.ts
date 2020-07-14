90b898926691a07b9e03d64838bb5621f726f65f
Alex King <alex@basal.dev>
2020-07-14 19:21:30 +0900

    remove toast from default state

--- src/store.ts
+++ src/store.ts
@@ -4,8 +4,8 @@ export const defaultState = {
   greeting: '🍖🍖🍖🍖',
   currentPage: { name: 'HOME', activePage: "/" },
   notification: {
-    text: "sharks loaded",
-    show: true
+    text: "",
+    show: false
   }
  }
 
@@ -13,7 +13,7 @@ export const defaultState = {
     'Home': '/',
     'Example Fetch': '/example-fetch',
  }
- //TODO clean up
+
  export const activePage = {
    "HOME": "/",
    "EXAMPLE_FETCH": "/example-fetch"
