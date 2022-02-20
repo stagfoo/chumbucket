b9439e0920ed0ad57146ee61df67025f4219a498
stagfoo <alexanderddking@gmail.com>
2022-02-20 18:31:24 +0900

    🧼　chore ░ Add typing state

--- src/store.ts
+++ src/store.ts
@@ -1,31 +1,41 @@
 import { reducer } from 'obake.js';
 
-export const defaultState = {
+type Notification = {
+  text: string,
+  show: boolean
+}
+
+export type Route = "HOME" | "EXAMPLE_FETCH"
+export type State = {
+  bucket: string,
+  currentPage: Route,
+  notification: Notification,
+  _update:(reducerName: string, data:any) => Promise<State>
+}
+
+
+export const defaultState: Omit<State, '_update'> = {
   bucket: '',
-  currentPage: { name: 'HOME', activePage: "/" },
+  currentPage: 'HOME',
   notification: {
     text: "",
     show: false
   }
  }
 
- export const routes = {
-    'Home': '/',
-    'Example Fetch': '/example-fetch',
+ export const ROUTES = {
+    'HOME': '/',
+    'EXAMPLE_FETCH': '/example-fetch',
  }
 
- export const activePage = {
-   "HOME": "/",
-   "EXAMPLE_FETCH": "/example-fetch"
- }
 export const reducers = {
-  updateCurrentPage: reducer((state, value: string) => {
-    state.currentPage = { name: value, activePage:activePage[value]  };
+  updateCurrentPage: reducer((state: State, value: Route) => {
+    state.currentPage = value;
   }),
-  updateBucket: reducer((state, value: string) => {
+  updateBucket: reducer((state: State, value: string) => {
     state.bucket = value;
   }),
-  updateNotification: reducer((state, value:{text: string, show: boolean}) => {
+  updateNotification: reducer((state: State, value:{text: string, show: boolean}) => {
     state.notification = value;
   }),
 }
