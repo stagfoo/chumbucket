98845bbb07e309d0690f23f00e60be442c8ae0bf
stagfoo <alexanderddking@gmail.com>
2022-02-20 20:11:48 +0900

    🍱　refactor ░ remove workbox because its annoying

--- src/index.ts
+++ src/index.ts
@@ -26,16 +26,3 @@ function renderer(newState: State) {
 // Start Router listener
 startRouters();
 baseStyles();
-
-if ('serviceWorker' in navigator) {
-	window.addEventListener('load', () => {
-		navigator.serviceWorker.register('./service-worker.js').then(
-			registration => {
-				console.log(`ServiceWorker registration successful with scope: ${registration.scope}`);
-			},
-			error => {
-				console.log(`ServiceWorker registration failed: ${error}`);
-			},
-		);
-	});
-}
